1205d15a307fdeb413636e654f6af8d0d9d2d798
Documenter.jl <documenter@juliadocs.github.io>
2024-08-14 22:37:39 +0000

    build based on 2fba869

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BioAtomsCount","category":"page"},{"location":"#BioAtomsCount","page":"Home","title":"BioAtomsCount","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BioAtomsCount.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BioAtomsCount]","category":"page"}]
+}
