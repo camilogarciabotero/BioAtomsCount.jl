dbb72fb4c34bd815d7e6e7552f27409898631e33
Camilo García <ca.garcia2@uniandes.edu.co>
2024-08-14 20:17:52 -0500

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,34 @@
-using Test
-using BioAtomsCount
+module BioAtomsCountTests
 
-@testset "BioAtomsCount.jl" begin
-    @test 1 == 1
+using BioSequences, BioAtomsCount
+
+using Aqua
+using Test
+using TestItems
+using TestItemRunner
+using JET
+
+@run_package_tests
+@testitem "countatoms" begin
+    using BioSequences, BioAtomsCount
+    
+    sorteddna = dna"ACGT"
+    sortedaa = aa"ARNDCQEGHILKMFPSTWYV"
+
+    @test countatoms(sorteddna) == BioAtomsCount.dnaweights
+    @test countatoms(sortedaa) == BioAtomsCount.aaweights
 end # BioAtomsCount.jl tests
+
+@testitem "Code quality" begin
+    using Aqua
+    Aqua.test_all(
+        BioAtomsCount; ambiguities=false, deps_compat=(check_extras=false,)
+    )
+end
+
+@testitem "Code linting" begin
+    using JET
+    JET.test_package(BioAtomsCount; target_defined_modules=true)
+end
+
+end # module BioAtomsCountTests
