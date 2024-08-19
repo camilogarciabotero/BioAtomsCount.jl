3f6ddb04d86c95da38d114dace4037bfc6dacd1c
Camilo García <ca.garcia2@uniandes.edu.co>
2024-08-19 16:36:54 -0500

    Add dispatch to othe BioSeqs

--- src/BioAtomsCount.jl
+++ src/BioAtomsCount.jl
@@ -3,10 +3,13 @@ module BioAtomsCount
 export countatoms
 
 using BioSequences: 
-  ACGT, DNA, SeqOrView,
+  ACGT, ACGU, DNA, RNA,
+  SeqOrView, NucleicAcidAlphabet, AminoAcidAlphabet,
   AA_A, AA_R, AA_N, AA_D, AA_C, AA_Q, AA_E, AA_G, AA_H, AA_I, AA_L, AA_K, AA_M, AA_F, AA_P, AA_S, AA_T, AA_W, AA_Y, AA_V
 
-const dnaweights = [
+const AA20 = (AA_A, AA_R, AA_N, AA_D, AA_C, AA_Q, AA_E, AA_G, AA_H, AA_I, AA_L, AA_K, AA_M, AA_F, AA_P, AA_S, AA_T, AA_W, AA_Y, AA_V)
+
+const DNAWEIGHTS = [
  #C H N O
   5 5 5 0; # Adenine
   4 5 3 1; # Cytosine
@@ -14,7 +17,15 @@ const dnaweights = [
   5 5 2 2; # Thymine
 ]
 
-const aaweights = [
+const RNAWEIGHTS = [
+ #C H N O
+  5 5 5 0; # Adenine
+  4 5 3 1; # Cytosine
+  5 5 5 1; # Guanine
+  4 4 2 2; # Uracil
+]
+
+const AAWEIGHTS = [
 # C  H  N  O  S
   3  7  1  2  0;  # A (Ala)
   6  14 4  2  0;  # R (Arg)
@@ -38,12 +49,27 @@ const aaweights = [
   5  11 1  2  0;  # V (Val)
 ]
 
-const AAs = (AA_A, AA_R, AA_N, AA_D, AA_C, AA_Q, AA_E, AA_G, AA_H, AA_I, AA_L, AA_K, AA_M, AA_F, AA_P, AA_S, AA_T, AA_W, AA_Y, AA_V)
+# function countatoms(seq::SeqOrView{A})::Matrix{Int64} where {A}
+#     atomweights, alph = eltype(seq) == DNA ? (dnaweights, ACGT) : (aaweights, AA20)
+#     counts = [count(x, seq) for x in isequal.(alph)]::Vector{Int64}
+#     return atomweights .* counts
+# end
 
-function countatoms(seq::SeqOrView{A})::Matrix{Int64} where {A}
-    atomweights, alph = eltype(seq) == DNA ? (dnaweights, ACGT) : (aaweights, AAs)
-    counts = [count(x, seq) for x in isequal.(alph)]::Vector{Int64}
-    return atomweights .* counts
+function countatoms(seq::SeqOrView{<:NucleicAcidAlphabet{N}}; vectorized=false)::Matrix{Int64} where {N}
+  counts = Vector{Int64}(undef, 4)
+  weights, alphabet = eltype(seq) == DNA ? (DNAWEIGHTS, ACGT) : (RNAWEIGHTS, ACGU)
+  @inbounds for i in 1:4
+    counts[i] = count(isequal(alphabet[i]), seq)
+  end
+  return weights .* counts
+end
+
+function countatoms(seq::SeqOrView{AminoAcidAlphabet}; vectorized=false)::Matrix{Int64}
+  counts = Vector{Int64}(undef, 20)
+  @inbounds for i in 1:20
+    counts[i] = count(isequal(AA20[i]), seq)
+  end
+  return AAWEIGHTS .* counts
 end
 
 end # BioAtomsCount
